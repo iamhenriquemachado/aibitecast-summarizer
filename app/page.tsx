66b6951aa424caafbde45e01c422bc9e406e84a5
Henrique Machado <heyhenriquecastro@gmail.com>
2025-03-18 17:59:38 -0300

    fix: adjust cors allowed endpoints and frontend endpoint request

--- app/page.tsx
+++ app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
     setProgress(0)
   
     try {
-      const response = await fetch('http://127.0.0.1:8000/youtube/summarize/', {
+      const response = await fetch('https://aibitecast.onrender.com/youtube/summarize', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
